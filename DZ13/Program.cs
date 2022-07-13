71673594b96868d153f1cf144108a019c4f94e2d
Ivan <375291073707ls@gmail.com>
2022-07-13 14:05:44 +0300

    Добавили папку с решение задачи №13

---
+++ DZ13/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6      */
+
+Console.WriteLine("Введите число: ");
+Random rand = new Random();
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+  Console.WriteLine("третья цифра " + numberText[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+}
